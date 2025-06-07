e960bb9711e2a3314fb9fe75d34e5ef0e754adab
Mohamedjassim17 <jassimjasra3@gmail.com>
2025-06-07 22:56:01 +0530

    Moved backend files to root for Render

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('MongoDB Error:', err));
+
+  
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
